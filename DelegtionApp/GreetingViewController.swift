196e3ccba7af147e8fadb912c4eb3712a1ea67a0
Alexander <Alexander29_90@icloud.com>
2024-08-28 17:10:13 +0500

    I wrote down the logic

--- DelegtionApp/GreetingViewController.swift
+++ DelegtionApp/GreetingViewController.swift
@@ -7,12 +7,23 @@
 
 import UIKit
 
-final class GreetingViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
-
+protocol ProfileViewControllerDelegate: AnyObject {
+    func setGreeting(for user: User)
 }
 
+final class GreetingViewController: UIViewController {
+
+    @IBOutlet var welcomeLabel: UILabel!
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let navigationVC = segue.destination as? UINavigationController
+        let profileVC = navigationVC?.topViewController as? ProfileViewController
+        profileVC?.delegate = self
+    }
+}
+
+extension GreetingViewController: ProfileViewControllerDelegate {
+    func setGreeting(for user: User) {
+        welcomeLabel.text = "Welcome, \(user.fullName)!"
+    }
+}
--- DelegtionApp/User.swift
+++ DelegtionApp/User.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+struct User {
+    var name: String
+    var surname: String
+    
+    var fullName: String {
+        "\(name) \(surname)"
+    }
+}
