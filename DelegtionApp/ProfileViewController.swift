c40134190dcb02862ece4d5a0cd7f81f91c89b49
Alexander <Alexander29_90@icloud.com>
2024-08-28 14:09:05 +0500

    Setup Storyboard

---
+++ DelegtionApp/ProfileViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileViewController.swift
+//  DelegtionApp
+//
+//  Created by Александр on 28.08.2024.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
